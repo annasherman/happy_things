41b6bf3c29fb919b74092bccf91e80018c22b08f
Anna Sherman <sherman.anna@gmail.com>
2015-12-06 19:25:58 -0600

    stuff

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+console.log('connected');
+
+window.onload=function(){
+
+
+$.ajax({
+  url: 'http://api.worldbank.org/countries/indicators/2.0.hoi.Int?per_page=10&date=1960:2015',
+  type: 'get',
+  dataType: 'jsonp',
+})
+.done(function(data) {
+  console.log("success");
+  console.table(data);
+})
+.fail(function() {
+  console.log("error");
+})
+.always(function() {
+  console.log("complete");
+});
+
+
+
+};
